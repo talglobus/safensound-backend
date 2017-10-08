eab7972dc1190c81cb3dd54ae3b5d717c3b1bb87
Tal Globus <talglobus@gmail.com>
2017-10-08 06:45:02 -0400

    Complete API black box model
    BEFORE, the API wasn't exactly usable, as it would not return correct
    output when interacted with from the outside world.
    
    AFTER, the black box model of the API is complete, in that while it may
    not correctly perform the needed computations to compute the correct
    results, it appears to do so, with output matching spec.

--- routes/api.js
+++ routes/api.js
@@ -1,49 +1,47 @@
 let express = require('express');
-let MongoClient = require('mongodb').MongoClient;
-let credentials = require('../config/mongo-credentials');
+let input = require('../api/in');
+let output = require('../api/out');
 let router = express.Router();
-let db = null;
-
-MongoClient.connect(credentials.url, (err, newDB) => {
-	if (err) throw err;
-	db = newDB;
-});
 
 /* Send data into the API */
 router.post('/send', (req, res, next) => {
-	if (!db) res.status(500).json({"error": "database not loaded"});
 
-	res.json({
-		"status": "ok",
-		"accepted": true
+	input(req.body, (err, inRes) => {
+		"use strict";
+		let responseObj = {};
+
+		if (err) return res.status(500).end(err);
+		else responseObj.status = "ok";
+
+		responseObj.accepted = !!inRes;
+
+		res.json(responseObj);
 	});
 });
 
 /* Retrieve data from the API */
 router.get('/receive', function(req, res, next) {
-	if (!db) res.status(500).json({"error": "database not loaded"});
-
+	"use strict";
 	let location = {
-		x: req.param('longitude'),
-		y: req.param('latitude')
+		x: req.body.longitude,
+		y: req.body.latitude
 	};
-	
-	// res.send('respond with a resource');
-	res.json({
-		time: Date.now(),
-		events: [{
-			title: "Suspected Shooting",
-			location: {
-				"lat" : 36.0918777,			// Latitude in degrees North
-				"lng" : -115.175245,			// Longitude in degrees East
-				"elev": 380					// Elevation in feet
-			},
-			place: "Mandalay Bay Hotel",
-			photo: "https://lh6.googleusercontent.com/proxy/0TSk0ZJ-zA7siqwmo9VO2QEFsusv8gsftlNTZCdAXEDExkTkApJXBcytlhRN9pC48Yjl8XzRhzMvJYl82HxDbDDHBC1fUmn50vAFdVK9bXdlTw8vUD85BCpm-w=s1536-k-no",
-			suggested_action: "",
-			time: "1507409670301"
-		}]
-	})
+
+	let id = req.body.id;
+
+	output(id, location, (err, outRes) => {
+		"use strict";
+		if (err) return res.status(500).end(err);
+
+		for (let i = 0; i < outRes.length; i++) {
+			delete outRes[i]._id;
+		}
+
+		res.json({
+			time: Date.now(),
+			events: outRes
+		});
+	});
 });
 
 module.exports = router;
